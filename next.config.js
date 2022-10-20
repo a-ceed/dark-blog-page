bd78382096b867143e4eb075fe7ce5dfbbdcc7aa
Aceed <forjetx@gmail.com>
2022-10-20 19:08:56 +0300

    next config js

--- next.config.js
+++ next.config.js
@@ -6,7 +6,9 @@ const nextConfig = {
     domains : ['localhost'],
     loader : "akamai",
     path: "",
-  }
+  },
+  basePath: "/dark-blog-page",
+  assetPrefix: "/dark-blog-page",
 }
 
 module.exports = nextConfig
