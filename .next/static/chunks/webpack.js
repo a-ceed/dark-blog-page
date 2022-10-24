5c1998b0c35ad71740cfb0aaf04c62723e30c33a
Aceed <forjetx@gmail.com>
2022-10-24 23:21:10 +0300

    Webmaster metateg

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5961775560c154fa"; }
+/******/ 		__webpack_require__.h = function() { return "1a7eecf8285f433b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
