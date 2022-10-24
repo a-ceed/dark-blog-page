4cf741bb73e7379fb66d7b148dfc1789a90faa48
Aceed <forjetx@gmail.com>
2022-10-24 23:07:29 +0300

    YM

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "caf6224530ebc70b"; }
+/******/ 		__webpack_require__.h = function() { return "5961775560c154fa"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
