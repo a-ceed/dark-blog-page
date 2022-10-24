4f7b7c17d6859098eeeba088f42c908733fc388f
Aceed <forjetx@gmail.com>
2022-10-24 20:41:38 +0300

    about

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "91624120fdf750c1"; }
+/******/ 		__webpack_require__.h = function() { return "4a27d3bd9cd32a28"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
