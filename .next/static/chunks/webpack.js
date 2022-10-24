a526eda2204bd527902089fce31d2136343fdc53
Aceed <forjetx@gmail.com>
2022-10-24 23:56:11 +0300

    add post inferno

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d0f6bf43c674da54"; }
+/******/ 		__webpack_require__.h = function() { return "25cdd9f4595eb548"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
