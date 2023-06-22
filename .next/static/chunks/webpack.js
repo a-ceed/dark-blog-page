d00e5518d1c0f3d46a80c1adf3de8324a39e323c
Aceed <forjetx@gmail.com>
2023-06-22 16:05:34 +0300

    add hudlit post

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "87f11b55b30dc635"; }
+/******/ 		__webpack_require__.h = function() { return "6537de7a93b2c303"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
