5fe61bc605dfcddad09cbe30da358dfb9584bbf2
a.sidorov@platformcraft.com <3L@vi!@TacA9DR>
2025-02-28 01:13:46 +0300

    cours v0.8.1

--- components/Navigation.js
+++ components/Navigation.js
@@ -10,6 +10,9 @@ const items3 = "Контакты"
      <li className="nav-home" role="menu-item">
          <Link href="/">{items1}</Link>
      </li>
+     <li className="nav-home" role="menu-item">
+         <Link href="/reality-ecology">Курс</Link>
+     </li>
     <li className="nav-home" role="menu-item">
         <Link href="/about">{items2}</Link>
     </li>
