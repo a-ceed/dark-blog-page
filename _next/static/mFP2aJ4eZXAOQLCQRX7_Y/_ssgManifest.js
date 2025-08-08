8f64ff92e2cd655c66d1c26bb98308579fed9a49
a-ceed <forjetx@gmail.com>
2025-08-08 21:17:02 +0000

    Deploying to gh-pages from @ a-ceed/dark-blog-page@222fb50993753e455ef0b1589d33e8163502d68a 🚀

--- _next/static/mFP2aJ4eZXAOQLCQRX7_Y/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Frgm","\u002Fblog","\u002Fair","\u002Fapplication2","\u002Fabout","\u002Fcontacts","\u002F","\u002Freality-ecology","\u002Fapplication1","\u002Fposts\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
---
+++ _next/static/w8i-gyrZDxJ8-t8BXGv4K/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Frgm","\u002Fblog","\u002Fabout","\u002Fapplication2","\u002Fcontacts","\u002Fair","\u002Freality-ecology","\u002Fcreativity","\u002F","\u002Fapplication1","\u002Fmedia\u002F[slug]","\u002Fposts\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
